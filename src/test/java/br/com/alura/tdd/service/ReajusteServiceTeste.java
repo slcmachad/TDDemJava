d732c02e15c85db3a9fec9de0758a67932c5f259
Salomao <salomao.machado.75@gmail.com>
2023-07-02 22:36:52 -0300

    <Teste> Criação: Criação de teste de acordo com o metodo TDD,crie o teste primeiro, crie o codigo depois.

--- src/test/java/br/com/alura/tdd/service/ReajusteServiceTeste.java
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTeste.java
@@ -1,5 +1,24 @@
 package br.com.alura.tdd.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
 public class ReajusteServiceTeste {
 
+	@Test
+	public void reajusteDeveriaSerTresPorCentoQuandoDesempenhoForADesejar() {
+		ReajusteService service = new ReajusteService;
+		Funcionario funcionario = new Funcionario("ana", LocalDate.now(), new BigDecimal("1000.00"));
+		
+		service.concedeReajuste(funcionario, Desempenho.A_DESEJAR);
+		
+		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
+	}
+	
 }
