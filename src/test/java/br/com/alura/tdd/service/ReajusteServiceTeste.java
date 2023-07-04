0524ae8199046b0e7c02c2aaf37b8916c338e85c
Salomao <salomao.machado.75@gmail.com>
2023-07-03 22:58:37 -0300

    <feat> refactor: refatorando o prorpio teste para remover duplicação de codigo.

--- src/test/java/br/com/alura/tdd/service/ReajusteServiceTeste.java
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTeste.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Desempenho;
@@ -12,31 +13,30 @@ import br.com.alura.tdd.modelo.Funcionario;
 
 public class ReajusteServiceTeste {
 
+	private ReajusteService service;
+	private Funcionario funcionario;
+	
+	@BeforeEach
+	public void inicializar() {
+		this.service = new ReajusteService();
+		this.funcionario = new Funcionario("ana", LocalDate.now(), new BigDecimal("1000.00"));
+	}
+	
 	@Test
 	public void reajusteDeveriaSerTresPorCentoQuandoDesempenhoForADesejar(){
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("ana", LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.concedeReajuste(funcionario, Desempenho.A_DESEJAR);
-		
 		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
 	}
+	
 	@Test
 	public void reajusteDeveriaSerQuinzePorCentoQuandoDesempenhoForBom(){
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("ana", LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.concedeReajuste(funcionario, Desempenho.BOM);
-		
 		assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
 	}
+	
 	@Test
 	public void reajusteDeveriaSerVintePorCentoQuandoDesempenhoForOtimo(){
-		ReajusteService service = new ReajusteService();
-		Funcionario funcionario = new Funcionario("ana", LocalDate.now(), new BigDecimal("1000.00"));
-		
 		service.concedeReajuste(funcionario, Desempenho.OTIMO);
-		
 		assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
 	}
 	
